83b401765a846fb937cd8816bd9b4a905d3eba02
Harsimran <harsimransinghbarki@pop-os.localdomain>
2021-09-08 16:44:36 +0530

    cleaned up js

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
-let camera_preview = document.getElementById("camera_popup");
-let main_preview = document.getElementById("main_preview");
-let gallery_item = document.querySelectorAll(".gallery_item");
-let camera_popup = document.getElementById("camera_popup_preview");
+// Initializing Selectors
+let popup = document.getElementById("camera_popup");
+let galleryMain = document.getElementById("gallery_preview");
+let galleryItem = document.querySelectorAll(".gallery_item");
+let popupImage = document.getElementById("popup_image");
 
-main_preview.addEventListener("mouseenter", () => {
+// Event Listeners
+
+// Image Preview
+galleryMain.addEventListener("mouseenter", () => {
   setPreview();
 });
 
-main_preview.addEventListener("mouseleave", () => {
+galleryMain.addEventListener("mouseleave", () => {
   setPreview();
 });
 
-let isPreview = false;
-let cameraStyle = camera_preview.style;
-
+// Preview Enable/Disable
 const setPreview = () => {
-  camera_popup.src = main_preview.src;
-  console.log(cameraStyle[0]);
-  if (!isPreview) {
-    cameraStyle.display = "block";
-    isPreview = true;
+  // Set Source
+  popupImage.src = galleryMain.src;
+
+  let display = popup.style.display;
+
+  if (display == "" || display == "none") {
+    popup.style.display = "block";
+    popup.style.opacity = "1";
   } else {
-    cameraStyle.display = "none";
-    isPreview = false;
+    popup.style.display = "none";
+    popup.style.opacity = "0";
   }
 };
 
-let currentActive = gallery_item[0];
+// Gallery Active CSS Class Enable/Disable
+let currentActive = galleryItem[0];
 
 // Add Event Listener To Each Element
-gallery_item.forEach((ele) => {
+galleryItem.forEach((ele) => {
   ele.addEventListener("click", (e) => {
-    let src = e.target.src;
-    main_preview.src = src;
+    galleryMain.src = e.target.src;
     ele.classList.add("gallery_item_active");
     currentActive = ele;
 
@@ -42,7 +47,7 @@ gallery_item.forEach((ele) => {
 
 // Update Hover Preview Of Previous Elements
 const updateAllPreview = () => {
-  gallery_item.forEach((ele) => {
+  galleryItem.forEach((ele) => {
     let curr = ele.getAttribute("data-imageId");
     if (curr !== currentActive.getAttribute("data-imageId")) {
       ele.classList.remove("gallery_item_active");
